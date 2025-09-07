8fae488e207758bfc921dee2b89ec92bd06e85f4
mateus andrade <mateusandrade092@gmail.com>
2025-09-06 21:50:29 -0300

    backend recebe informações do gestor e insere no banco de dados, devolve a informação para o front que foi realizado com sucesso

--- Back-end/app.py
+++ Back-end/app.py
@@ -42,6 +42,37 @@ def index():
 
 
 
+@app.route('/register_gestor', methods = ['POST'])
+def register_gestor():
+
+    if request.method == "POST":
+        data = request.get_json()
+
+        name = data.get('name')
+        user = data.get('user')
+        turn = data.get('turn')
+        password = data.get('password')
+                                                                        #acima temos as primeiras informações que o sistema recebe: cadastro dos gestores. Ele deve receber: nome, usuário, turno em que trabalha e senha. Ao receber as informações do front-end ele deve inseri-las no banco de dados
+
+        conn = get_db_connection()
+        cursor= conn.cursor()
+
+        cursor.execute(''' INSERT INTO gestor (
+                                                name,
+                                                username,
+                                                password,
+                                                turn) VALUES (
+                                                            %s,
+                                                            %s,
+                                                            %s,
+                                                            %s)''',(name,user,password,turn))
+
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return jsonify("novo Gestor cadastrado com sucesso")
 
 
 
