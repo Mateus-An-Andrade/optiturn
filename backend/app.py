248cf982c2132b6a7e21d3a8edfa001f2f12c554
mateus andrade <mateusandrade092@gmail.com>
2025-11-07 21:36:11 -0300

    ALTERAÇÕES: Mudando o nome da coluna de ID para o nome correto

--- backend/app.py
+++ backend/app.py
@@ -368,7 +368,7 @@ def map():
         elif button_complete_clicked == True:
             cursor.execute('''UPDATE production SET status = 'Concluida' WHERE activity_id = %s''', (activity_id_status,))
             
-            cursor.execute('''UPDATE turn SET status = 'Concluida' WHERE activity_id = %s''', (activity_id_status,))
+            cursor.execute('''UPDATE turn SET status = 'Concluida' WHERE activities_id = %s''', (activity_id_status,))
 
 
             cursor.execute('''INSERT INTO history_production (activity_id, 
