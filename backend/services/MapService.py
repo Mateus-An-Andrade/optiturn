fdfb051065f50a289492b69460162258be0affbe
mateus andrade <mateusandrade092@gmail.com>
2026-03-11 22:11:14 -0300

    ALTERAÇÕES: refatoração de envio dos dados para o front. Back end enviando dados em JSON

--- backend/services/MapService.py
+++ backend/services/MapService.py
@@ -1,6 +1,22 @@
 from repositories.mapMenu_repositories import queryAndUpdateMap
 
 def dataMap():
-    data = queryAndUpdateMap()
-    
-    return(data)
+    data_query = queryAndUpdateMap()
+
+    array_data_map = []
+
+    for d in data_query:
+
+        id_Op, name_Op, task_id, task_status, task_title, task_text, task_priority = d
+        
+        jsonMap = {   "id_op":id_Op,
+                      "name_Op":name_Op,
+                      "task_id":task_id,
+                      "task_status": task_status,
+                      "task_title":task_title,
+                      "task_text":task_text,
+                      "task_priority":task_priority} 
+     
+        array_data_map.append(jsonMap)  
+
+    return(array_data_map)
