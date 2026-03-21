5da43ca3c09104cc3a086ea4e523e9fe186062f0
mateus andrade <mateusandrade092@gmail.com>
2026-03-21 19:44:54 -0300

    alterações: dados enviados conforme a estrutura front end

--- backend/services/reportShiftService.py
+++ backend/services/reportShiftService.py
@@ -3,25 +3,34 @@ from repositories.shiftReport import createReportShift
 def report_service():
     reportData = createReportShift()
     
-    arrayReport =[]
+
+    arrayLenOps = []
+    arrayLenTasks =[]
+    arrayCompleteTasks =[]
+    arrayIncompleteTasks = []
 
     for data in reportData:
-        print(data)
-        idOp= data[0],
-        idTask = data[1],
-        statusTask = data[2],
-        dateTask= data[3],
+        idOp= data[0]
+        idTask = data[1]
+        statusTask = data[2]
+        dateTask= data[3]
 
-      
-        
+        if statusTask == "CONCLUÍDO":
+            arrayCompleteTasks.append(statusTask)
 
-        arrayReport.append({"idOps": idOp,
-                  "idTask": idTask,
-                  "status": statusTask,
-                  "dateTask": dateTask,
-                  "numberTasks": len(reportData)
-                  })
-   
+        elif statusTask == "PENDENTE":
+            arrayIncompleteTasks.append(statusTask)
+
+
+        if idOp not in arrayLenOps:
+            arrayLenOps.append(idOp)
+
+        if idTask not in arrayLenTasks:
+            arrayLenTasks.append(idTask)
     
-
-    return (arrayReport)
+    return ({"numberOperators": len(arrayLenOps),
+             "numberTasksCreated": len(arrayLenTasks),
+             "dateReport": dateTask,
+             "numberCompleteTasks": len(arrayCompleteTasks),
+             "numberIncompleteTasks": len(arrayIncompleteTasks)
+             })
