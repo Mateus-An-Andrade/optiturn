015468c4cfce4b23daacf5a38b9e0542579cc281
mateus andrade <mateusandrade092@gmail.com>
2026-03-28 21:33:42 -0300

    Query de consulta de diaria para relatório de turnos atualizada

--- backend/repositories/shiftReport.py
+++ backend/repositories/shiftReport.py
@@ -4,10 +4,14 @@ def createReportShift():
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    cursor.execute('''SELECT operator_id, activity_id, status, create_date FROM production''')
-
+    cursor.execute('''
+        SELECT operator_id, activity_id, status, create_date
+        FROM production
+        WHERE create_date >= CURRENT_DATE
+        AND create_date < CURRENT_DATE + INTERVAL '1 day'
+    ''')
     report_data = cursor.fetchall()
 
     cursor.close()
-    
+    print(report_data)
     return (report_data)
