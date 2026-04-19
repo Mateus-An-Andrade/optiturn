ae0e759f59abee64942d5592a131ad164b9f8a62
mateus andrade <mateusandrade092@gmail.com>
2026-04-18 21:16:31 -0300

    ALTERAÇÃO: atualização! operador só tem acesso ao menu mapa para acompanhar as tarefas! update UI informa se o usuário logado tem acesso de operador ou acesso de gestor!

--- Front-end/static/js/features/updateUI.js
+++ Front-end/static/js/features/updateUI.js
@@ -1,32 +1,50 @@
 import { API_BASE_URL } from "../features/config.js";
 
 export async function update_UI(){
-    try{
-        const response = await fetch(`${API_BASE_URL}/main`,{
-            method: "POST",
-            credentials: "include"
-        });
-           
-        
-        if (!response.ok) throw new Error("Falha ao carregar dados")
-        
-        const data = await response.json()
-
+   
+     try{
+        let type_acess_system = document.getElementById("type_acess_system")
         let name_gestor = document.getElementById("name_gestor_interface")
         let shift_gestor = document.getElementById("turn_gestor_interface")
 
-        name_gestor.textContent = data.name
-        shift_gestor.textContent = data.shift
-        window.currentUser = data
-        sessionStorage.setItem("user", JSON.stringify(data))
+         const response = await fetch(`${API_BASE_URL}/main`,{
+             method: "POST",
+             credentials: "include"
+         });
+         
+         
+        if (!response.ok) throw new Error("Falha ao carregar dados")
+        const data = await response.json()
         console.log("esses são os dados da session:",data)
 
+        if(data.type_access === "leader"){            
+            name_gestor.textContent = data.name
+            shift_gestor.textContent = data.shift
+            window.currentUser = data
 
-    }catch (err) {
-        console.error("Erro ao atualizar UI:", err);
-    }
+        }else if(data.type_access === "operator"){
+            type_acess_system.innerText = "Olá operador: "
+            name_gestor.textContent = data.name
+            shift_gestor.textContent = data.shift
 
-}                                                           
+            let menu_buttons = document.querySelectorAll(".botoes_menu button")
+
+                menu_buttons.forEach(btn =>{
+                    console.log(btn)
+                    if(btn.id !== "map_button"){
+                        btn.style.display = "none"
+                    }
+                })
+        }
+     
+        window.currentUser = data
+        sessionStorage.setItem("user", JSON.stringify(data))
+
+     }catch (err) {
+         console.error("Erro ao atualizar UI:", err);
+     }
+
+}                                                          
 
                                                                         //acima temos a função de atualização da interface, ou seja, ao logar ela será a responsável por mostrar o nome e o turno do gestor, assim como os icones presentes na página
     
