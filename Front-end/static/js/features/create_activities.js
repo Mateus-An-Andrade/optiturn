127db3a45b49d91ef0026ce170db4804ff392ad2
mateus andrade <mateusandrade092@gmail.com>
2026-09-15 17:31:32 -0300

    ALTERAÇÕES: 1- Exclusão da estrutura fixa da tabela de demandas no HTML(Agora criada dinâmicamente), 2- Refinamento de estilização da tabela de demandas na tabela (agora somente com a borda inferior e preenchimento menor no hover), 3- criação dinâmica da tabela, com títulos descrições e importância, assim como os checkbox com index para diferencia o que será clicado. Comentários atualizados

--- Front-end/static/js/features/create_activities.js
+++ Front-end/static/js/features/create_activities.js
@@ -3,23 +3,20 @@ import { close_windows } from './closeWindows.js';
 import { API_BASE_URL } from "./config.js";
 import { show_confirmation_menssage } from "./confirmMenssage.js    "
 
-export function create_activities(){
+export function create_activities(dataDemand){
     let time = 0
     let menu_activity = document.getElementById("menu_activities")
     let confirmation_register_button = document.getElementById("confirmation_button_activity")
+    let permanentDemandButton = document.getElementById("permanentDemandButton")
 
-                                                                                     /*acima temos as variaveis que armazenam as classes e IDs do elementos HTML referentes ao menu de atividades */
+                                                                    /*acima temos as variaveis que armazenam as classes e IDs do elementos HTML referentes ao menu de atividades */
     open_menu("activity_button","menu_activities","flex")
     open_inner_option("periodicDemandButton","periodicDemandMenuCreate","flex", "demandMenu", "permanentDemandMenu")
-    open_inner_option("permanentDemandButton", "permanentDemandMenu", "block", "demandMenu", "periodicDemandMenuCreate")
+    open_inner_option("permanentDemandButton", "permanentDemandMenu", "flex", "demandMenu", "periodicDemandMenuCreate")
     close_windows("menu_activities",menu_activity )
 
 
-                                                                                    //acima temos as interações basicas do menu que permite que ele seja aberto para a interação do usuário e a mudança da url do menu.
-
-
-
-                                                                                    /*acima temos as variaveis que recebem os campos referentes as informações que o gestor vai imputar referente as tarefas criadas, para enviar para o backend*/
+                                                                    //acima temos as interações basicas do menu que permite que ele seja aberto para a interação do usuário e a mudança da url do menu.
 
     const importance_task = document.querySelectorAll(".priority_btn")
     let dataImportance = null
@@ -34,11 +31,8 @@ export function create_activities(){
         const title_task_created = document.getElementById("title_task_created").value
         const descreption_task_text = document.getElementById("descreption_task_text").value
 
-
-        
-
-
         console.log("tarefa criada:", title_task_created,descreption_task_text,dataImportance)
+
         fetch(`${API_BASE_URL}/create/activity`,{
             method: "POST",
             headers:{
@@ -61,10 +55,113 @@ export function create_activities(){
                 }else{
                     show_confirmation_menssage(realResponse.message, time=3000,menu_activity, "error")
                 }
+
+                                                                         //acima temos a interação do usuário que mostra a mensagem de sucesso quando a tarefa é registrada.
             })
        
     })
 
+                                            /*acima temos as variaveis que recebem os campos referentes as informações que o gestor vai imputar referente as tarefas criadas, para enviar para o backend, assim como o fetch para enviar os dados ao backend*/
 
-                                                                                    //acima temos a interação do usuário que mostra a mensagem de sucesso quando a tarefa é registrada.
+
+    permanentDemandButton.addEventListener("click", function(){
+        let permanentDemandMenu = document.getElementById("permanentDemandMenu")
+        fetch(`${API_BASE_URL}/create/permanentActivity`,{  
+            method: "POST",
+            headers:{
+                'Content-type': 'application/json',
+            },
+            credentials: "include",
+            body:JSON.stringify({data:dataDemand})
+        })
+
+        .then(response => response.json())
+        .then(data => {
+            const oldContainers = permanentDemandMenu.querySelectorAll('.permanentDemandMenuContainer');
+            oldContainers.forEach(el => el.remove());
+
+            const tableContainer = document.createElement("div");
+            tableContainer.classList.add('permanentDemandMenuContainer');
+
+            tableContainer.style.display = "grid";
+            tableContainer.style.backgroundColor = "black";
+
+            const table = document.createElement("table")
+            const tableHead = document.createElement("thead")
+            const tableHeadRow = document.createElement("tr")
+                  tableHead.id="headDemandTable"
+            const tableBody = document.createElement("tbody")
+
+            const thTitle = document.createElement("th");
+            const thDescription = document.createElement("th");
+            const thImportance = document.createElement("th");
+            const thSelection = document.createElement("th");
+
+                                                     //Criação do container que receberá a tabela de demandas fixas e da tabela que vai exibir os dados na tela de acordo com o que vier da base de dados como fixo. Criação da tabela,head, row, body
+
+
+            thTitle.append("Título")
+            thDescription.append("Descrição")
+            thImportance.append("Importância")
+            thSelection.append("Subir para produção?")
+
+
+            tableHeadRow.append(thTitle);
+            tableHeadRow.append(thDescription);
+            tableHeadRow.append(thImportance);
+            tableHeadRow.append(thSelection);
+
+            tableHead.append(tableHeadRow);
+                                                        //Criação de textos que vão no cabeçalho da tabela e inserção dos mesmos no elemento head da tabela.
+            data.forEach((e, index) =>{
+                const tableRow = document.createElement("tr") //1
+
+                const tdTitle = document.createElement("td");//2
+                const tdDescription = document.createElement("td");//2
+                const tdImportance = document.createElement("td");//2
+                const tdSelection = document.createElement("td");//2
+
+
+                const checkBox = document.createElement("input")//2.1
+                checkBox.type = "checkbox"//2.1
+                checkBox.classList = "upProduction"//2.1
+                checkBox.dataset.index = index//2.1
+            
+
+                tdTitle.textContent = e[0]; //3
+                tdDescription.textContent = e[1];//3
+                tdImportance.textContent = e[2];//3
+                tdSelection.appendChild(checkBox)
+
+
+                                                        //Loop que percorre os dados retornados do servidor indexando cada linha pelo checkbox: Para cada dado retornado: 1-crie uma nova linha, 2-crie esses elementos nas linhas criadas, 2.1- criação do checkbox, definição da sua classe e indicação de index 3- pegue esses indices e coloque os dados deles nos dados correspondentes das linhas    
+
+                tableRow.appendChild(tdTitle);
+                tableRow.appendChild(tdDescription);
+                tableRow.appendChild(tdImportance);
+                tableRow.appendChild(tdSelection);
+
+                tableBody.appendChild(tableRow);
+
+                                                        //Adição dos dados: Titulo, descrição, importância e seleção nas linhas da tabela.
+            })
+            
+            table.appendChild(tableHead);
+            table.appendChild(tableBody);
+
+            tableContainer.appendChild(table);
+            permanentDemandMenu.appendChild(tableContainer);
+
+                                                                    //Criação dos elementos da tabela.
+            
+            console.log("esses são os dados",data)})
+
+
+
+                                            /*Fetch que recupera do backend as demandas permanentes ou ciclica. Ao clicar em demandas permanentes ele já recebe do servidor todas as que são cadastradas com o status True de demandas permanentes.*/
+
+    })
+
+    
+                                                                
 }
